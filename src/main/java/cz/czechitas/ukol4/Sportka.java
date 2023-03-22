4eaa8344372d6e5c09abf1df79dd82a836fa10ab
PavlinaRacakova <pavlina.kupkova@email.cz>
2023-03-21 19:33:13 -0600

    final

--- src/main/java/cz/czechitas/ukol4/Sportka.java
+++ src/main/java/cz/czechitas/ukol4/Sportka.java
@@ -8,46 +8,43 @@ import java.util.List;
  * Třída pro losování Sportky.  a
  */
 public class Sportka {
-  private final List<Integer> osudi = new ArrayList<>();
+    private final List<Integer> osudi = new ArrayList<>();
 
-  public Sportka() {
-    //TODO naplnit osudí čísly 1 až 49.
-    for (int i = 0; i < 49; i++) {
-      osudi.add(i + 1);
+    public Sportka() {
+        for (int i = 0; i < 49; i++) {
+            osudi.add(i + 1);
+        }
     }
-  }
 
-  /**
-   * Zamíchá osudí.
-   *
-   * @see Collections#shuffle(List)
-   */
-  public void zamichat() {
-    //TODO zamíchat osudí
-    Collections.shuffle(osudi);
-  }
-
-  /**
-   * Vrátí seznam s 6 vylosovanými čísly.
-   *
-   * @return Seznam vylosovaných čísel.
-   *
-   * @see List#subList(int, int)
-   */
-  public List<Integer> vylosovanaCisla() {
-    List<Integer> vylosovanaCisla = new ArrayList<>(6);
-    for (int i = 0; i < 6; i++) {
-      vylosovanaCisla.add(osudi.get(i));
+    /**
+     * Zamíchá osudí.
+     *
+     * @see Collections#shuffle(List)
+     */
+    public void zamichat() {
+        Collections.shuffle(osudi);
     }
-    return vylosovanaCisla;
-  }
 
-  /**
-   * Vrátí sedmé vylosované číslo (dodatkové číslo)
-   *
-   * @return Dodatkové číslo.
-   */
-  public Integer dodatkoveCislo() {
-    return osudi.get(6);
-  }
+    /**
+     * Vrátí seznam s 6 vylosovanými čísly.
+     *
+     * @return Seznam vylosovaných čísel.
+     * @see List#subList(int, int)
+     */
+    public List<Integer> vylosovanaCisla() {
+        List<Integer> vylosovanaCisla = new ArrayList<>(6);
+        for (int i = 0; i < 6; i++) {
+            vylosovanaCisla.add(osudi.get(i));
+        }
+        return vylosovanaCisla;
+    }
+
+    /**
+     * Vrátí sedmé vylosované číslo (dodatkové číslo)
+     *
+     * @return Dodatkové číslo.
+     */
+    public Integer dodatkoveCislo() {
+        return osudi.get(6);
+    }
 }
